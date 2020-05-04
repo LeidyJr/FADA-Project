c650bd3abb85fda77cdf0ad6daef536ae96ec169
LeidyJr <rivera.leidy@correounivalle.edu.co>
2020-05-04 00:31:05 -0500

    cambios

--- prueba2.py
+++ prueba2.py
@@ -1,8 +1,11 @@
 n = 6 #Número de animales
 m = 3 #Número de partes
 k = 2 #Número de escenas por parte
+# escena = conjunto de 3 animales
 
 animales = {'ciempies':1, 'libelula':2, 'gato':3, 'perro':4, 'tapir':5, 'nutria':6} # Animales {nombre:grandeza}
+
+
 
 apertura = [
 ['tapir', 'nutria', 'perro'],
@@ -11,11 +14,16 @@ apertura = [
 ['gato', 'ciempies', 'libelula']
 ]
 
+apertura_valores = []
 i = 0
 for j in range(len(apertura)):
 	for x in apertura[i]:
 		if x in animales:
 			print(animales[x])
+			apertura_valores.append(animales[x])
 	i += 1
 
-
+print("--------------------")
+print(apertura_valores)
+#double_dict1 = {k:v*2 for (k,v) in animales.items()}
+#print(double_dict1)
