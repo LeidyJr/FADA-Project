8002911b996b4bfe1ce541f0a484c3b58795a109
franco026 <franco.kevin@correounivalle.edu.co>
2020-05-06 16:52:33 -0500

    Update nlogn.py

--- nlogn.py
+++ nlogn.py
@@ -1,87 +1,110 @@
 
 # Python program for implementation of MergeSort 
-def mergeSort(arr):
-	if len(arr) >1: 
-		mid = len(arr)//2 #Finding the mid of the array 
-		L = arr[:mid] # Dividing the array elements  
-		R = arr[mid:] # into 2 halves 
-
-		mergeSort(L) # Sorting the first half 
-		mergeSort(R) # Sorting the second half 
-
-		i = j = k = 0
-		
-		# Copy data to temp arrays L[] and R[] 
-		while i < len(L) and j < len(R): 
-			if L[i] < R[j]: 
-				arr[k] = L[i] 
-				i+=1
-			else: 
-				arr[k] = R[j] 
-				j+=1
-			k+=1
-		
-		# Checking if any element was left 
-		while i < len(L): 
-			arr[k] = L[i] 
-			i+=1
-			k+=1
-		
-		while j < len(R): 
-			arr[k] = R[j] 
-			j+=1
-			k+=1
+def mergeSort(arr): 
+    if len(arr) >1: 
+        mid = len(arr)//2 #Finding the mid of the array 
+        L = arr[:mid] # Dividing the array elements  
+        R = arr[mid:] # into 2 halves 
+        mergeSort(L) # Sorting the first half 
+        mergeSort(R) # Sorting the second half 
+  
+        i = j = k = 0
+          
+        # Copy data to temp arrays L[] and R[] 
+        while i < len(L) and j < len(R): 
+            if TamañoEscenarios(L[i]) < TamañoEscenarios(R[j]): 
+                arr[k] = L[i] 
+                i+=1
+            else: 
+                arr[k] = R[j] 
+                j+=1
+            k+=1
+          
+        # Checking if any element was left 
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+=1
+            k+=1
+          
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+=1
+            k+=1
+  
 # Code to print the list 
-def printList(arr): 
-	for i in range(len(arr)):         
-		print(arr[i],end=" ") 
-	print() 
+def  OrganizarEscenarios(arr,low,high): 
 
+    if DiccionarioZoo.get(arr[low]) < DiccionarioZoo.get(arr[high]):
+        mini = DiccionarioZoo.get(arr[low])
+        maxi = DiccionarioZoo.get(arr[high])
+        
+    else:
+        maxi = DiccionarioZoo.get(arr[low])
+        mini= DiccionarioZoo.get(arr[high])
+        arr[low],arr[high] = arr[high],arr[low]
 
-def Change(parts):
-	
-	DiccionarioPartes = [[(Zoo.get(p),p)  for p in parts[q]] for q in range(len(parts))]
+    for j in range(low+1, high):
+        if  DiccionarioZoo.get(arr[j]) >= maxi: 
+            arr[j],arr[high] = arr[high],arr[j]
+        else:
+            if DiccionarioZoo.get(arr[j]) <= mini:
+                arr[j],arr[low] = arr[low],arr[j]
+    
 
-	TamañosEscenas = [[ p[0]  for p in DiccionarioPartes[q]]for q in range(len(DiccionarioPartes))]
-	
-	TotaltamañodePartes= [ sum(p) for p in TamañosEscenas]
+def TamañoEscenarios(arr):
+    tamaño = 0;
+    try:
+        for j in range(0,2):
+            tamaño +=DiccionarioZoo.get(arr[j])
+    except:
+        for k in range(len(arr)):
+            for j in range(0,2):
+                tamaño +=DiccionarioZoo.get(arr[k][j])
+    
+    return tamaño
+   
+    
 
-	diccionario = (dict(zip(TotaltamañodePartes, DiccionarioPartes)))
-	
-	tupla = list(diccionario.items())
-
-	for x in range(len(tupla)):
-		mergeSort(tupla)
-	#print(tupla)
-
-	newlist = [tupla[p][1] for p in range(len(tupla))]
-
-	for x in range(len(newlist)):
-		mergeSort(newlist[x])
-	a = [{p[1]:p[0] for p in newlist[q]} for q in range(len(newlist))]
-	
-	
-	Final = [[y for y in a[x].keys()] for x in range(len(a)) ]
-	print(Final)
-	#
-	#
-
-
-
-	
-
-# driver code to test the above code 
+    # driver code to test the above code 
 if __name__ == '__main__': 
-	n = 6
-	m = 3
-	k = 2
-	Zoo = {'Gato': 3, 'Libelula': 2, 'Ciempies': 1, 'Nutria': 6, 'Perro': 4, 'Tapir':5}
-	
-	apertura = [['Tapir', 'Nutria', 'Perro'], ['Tapir', 'Perro' ,'Gato'], ['Ciempies', 'Tapir', 'Gato'], ['Gato', 'Ciempies', 'Libelula']]
+    n = 6
+    m = 3
+    k = 2
 
-	parte1 = [['Tapir', 'Nutria', 'Perro'], ['Ciempies', 'Tapir', 'Gato']]
+    animales = ['Gato', 'Libelula', 'Ciempies', 'Nutria', 'Perro', 'Tapir'];
+    grandezas = [3, 2, 1, 6, 4, 5];
 
-	parte2 = [['Gato', 'Ciempies', 'Libelula'], ['Tapir', 'Perro' 'Gato']]
+    DiccionarioZoo= {'Gato': 3, 'Libelula': 2, 'Ciempies': 1, 'Nutria': 6, 'Perro': 4, 'Tapir':5}
+    Zoo2 = {3:'Gato',  2:'Libelula', 1:'Ciempies', 6:'Nutria', 4:'Perro', 5:'Tapir'}
 
-	Change(apertura);
-	
+    ##OrdenGA = Orden(animales,grandezas)
+        
+    apertura = [['Tapir', 'Nutria', 'Perro'], ['Tapir', 'Perro' ,'Gato'], ['Ciempies', 'Tapir', 'Gato'], ['Gato', 'Ciempies', 'Libelula']]
+
+    parte1 = [['Tapir', 'Nutria', 'Perro'], ['Ciempies', 'Tapir', 'Gato']]
+
+    parte2 = [['Gato', 'Ciempies', 'Libelula'], ['Tapir', 'Perro','Gato']]
+
+    
+    
+
+    
+    for i in range((m-1)*k):
+        OrganizarEscenarios(apertura[i],0,2)
+    
+    for i in range(0,k):
+        OrganizarEscenarios(parte1[i],0,2)
+        OrganizarEscenarios(parte2[i],0,2)
+    
+    
+    
+    mergeSort(apertura)
+    mergeSort(parte1)
+    mergeSort(parte2)
+    
+    parts = [parte1]+[parte2]
+    mergeSort(parts)
+
+    print('apertura ' + str(apertura))
+    for i in range(0,m-1):
+        print('Parte '+str(i+1)+ str(parts[i]))
