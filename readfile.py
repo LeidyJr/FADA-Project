2e55683cf68612976942ca77041a93bb3f504d44
franco026 <franco.kevin@correounivalle.edu.co>
2020-05-08 00:51:20 -0500

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -1,8 +1,12 @@
 lista = []
+apertura = []
+parts = []
+x=0
+y=3
 archivo = open('your_file.txt', 'r')
 with open('your_file.txt', 'r') as procfile:
     for line in procfile:
-        if line.replace(';','').split() != []:
+        if line.split() != []:
             lista.append(line.replace(';','').replace('{','[').replace('}',']').replace("'",'').split()[2:])
 
 n = int(lista[0][0])
@@ -12,4 +16,25 @@ k = int(lista[2][0])
 animales = [lista[3][i].replace('[','').replace(',','').replace(']','') for i in range(n-1)]
 grandeza = [lista[4][i].replace('[','').replace(',','').replace(']','') for i in range(n-1)]
 
-apertura = [[lista[5][i].replace('[','').replace(',','').replace(']','') for i in range((m-1)*k)] for j in range(n-1)]
+for g in range((m-1)*k):
+    apertura.append([])
+    for i in range(x,y):
+        apertura[g].append((lista[5][i].replace('[','').replace(',','').replace(']','')))
+    x +=3
+    y +=3
+
+x = 0
+y = 3
+
+for r in range(m-1):
+    parts.append([])
+    for t in range(k):
+        parts[r].append([])
+        for i in range(x,y):
+            parts[r][t].append((lista[6+r][i].replace('[','').replace(',','').replace(']','')))
+        x += 3
+        y += 3
+    x = 0
+    y = 3
+#parts = [[lista[6][i].replace('[','').replace(',','').replace(']','') for i in range(3)] for j in range(m-1)]
+print(parts)
