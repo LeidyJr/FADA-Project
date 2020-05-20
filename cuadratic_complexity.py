a20de03005f55d1e5260a3101ec339563ea51b71
Leidy Rivera <rivera.leidy@correounivalle.edu.co>
2020-05-20 00:54:52 -0500

    fixing errors and adding test

--- cuadratic_complexity.py
+++ cuadratic_complexity.py
@@ -50,14 +50,14 @@ def TamañoEscenas(arr):
     global promedio
     global k
     try:
-        for j in range(0,k):
+        for j in range(0,2):
             
             tamaño +=DiccionarioZoo.get(arr[j])
         GrandezaMaxEscena(tamaño,arr)
         GrandezaMinEscena(tamaño,arr)
     except:
         for p in range(len(arr)):
-            for j in range(0,k):
+            for j in range(0,2):
                 tamaño +=DiccionarioZoo.get(arr[p][j])
             GrandezaMaxEscena(tamaño,arr[p])
             GrandezaMinEscena(tamaño,arr[p])
